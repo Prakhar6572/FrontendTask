0b6f4024557865df4c3f4c5718edd5ac6f50ce68
Prakhar Shukla <113130203+Prakhar6572@users.noreply.github.com>
2024-06-22 00:53:36 +0530

    Update ReadOnlyRow.js

--- src/components/ReadOnlyRow.js
+++ src/components/ReadOnlyRow.js
@@ -9,7 +9,17 @@ const ReadOnlyRow = ({ contact, handleEditClick, handleDeleteClick }) => {
       <td>
         <button
           type="button"
-          onClick={(event) => handleEditClick(event, contact)}
+          onClick={(event) => handleEditClick(event, contact)} 
+          style={{backgroundColor:'rgb(78, 177, 235)',color:'#ffffff',fontSize:'20px',
+            width: '100px',
+            height: '36px',
+            paddingLeft: '1px',
+            paddingTop: '5px',
+            paddingBottom: '5px',
+            paddingRight: '1px',
+            border: 'none',
+            borderRadius: '5px',
+            textAlign: 'center',}}
         >
           Edit
         </button>
@@ -18,7 +28,16 @@ const ReadOnlyRow = ({ contact, handleEditClick, handleDeleteClick }) => {
         if (confirmBox === true) {
         handleDeleteClick(contact.id)
     }
-    }}>
+    }} style={{backgroundColor:'rgb(237, 54, 54)',color:'#ffffff',fontSize:'20px',
+      width: '100px',
+      height: '36px',
+      paddingLeft: '1px',
+      paddingTop: '5px',
+      paddingBottom: '5px',
+      paddingRight: '1px',
+      border: 'none',
+      borderRadius: '5px',
+      textAlign: 'center'}} >
           Delete
         </button>
       </td>
